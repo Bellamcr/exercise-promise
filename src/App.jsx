3b865ada2012d9cf23b57136e0e7c39ace51ba5a
Bellamcr <izabella.mcr@gmail.com>
2023-11-15 20:42:30 -0700

    useState and .then

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+    const [products, setProducts] = useState([]);
+
   useEffect(() => {
-    console.log("UseEffect starts")
-    fetch(
-      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
-    );
     console.log("UseEffect starts");
+    fetch(
+      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+    ) 
+      .then((response) => response.json())
+      .then ((data) => setProducts(data));
+    console.log("UseEffect ended");
   }, [] );
 
+    const list = products.map((product) => <li key={product.name}>{product.name}</li>)
   return (
-    <>
-      <div>        
-        Hello!
-      </div>
-     
+    <> 
+      Hello!
+        <div>
+          <ul>
+            {list} 
+          </ul>
+        </div>
     </>
   )
 }
